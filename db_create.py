fd7698eac3ac88098c9cac4fd48fd92ae4fe468a
Mikolaj Cymcyk <mic5@hi.is>
2020-02-15 16:35:36 +0000

    db setup

---
+++ db_create.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import json
+import random
+import copy
+
+users = ["audurmargret", "booninja", "Stimmikex", "thth16", "whackingcheese"]
+user_entry = {
+    "username": None,
+    "password": None,
+    "time": None,
+    "cookie_id": None,
+    "session_id": None
+}
+
+db = json.load(open("database.json", "r"))
+
+db["company_id"] = 1
+db["clicks"] = len(users)
+db["number_phished"] = len(users)
+
+for i in range(len(users)):
+    item = copy.deepcopy(user_entry)
+    item["username"] = users[i]
+    item["password"] = "secret"
+    item["time"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    item["cookie_id"] = random.randint(0, 999999999)
+    item["session_id"] = random.randint(0, 999999999)
+    db["phished"].append(item)
+
+json.dump(db, open("database.json", "w"))
