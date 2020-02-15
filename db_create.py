84f9ce8cf21e114e511e0c7b06ea966ac3f8f4be
Mikolaj Cymcyk <mic5@hi.is>
2020-02-15 18:41:32 +0000

    db update

--- db_create.py
+++ db_create.py
@@ -3,28 +3,51 @@ import json
 import random
 import copy
 
-users = ["audurmargret", "booninja", "Stimmikex", "thth16", "whackingcheese"]
-user_entry = {
+COMP_NUMB = 10
+COMP_USER_MIN = 10
+COMP_USER_MAX = 100
+EMP_CLICK_RATE = 0.4
+EMP_PHISH_RATE = 0.5
+
+c = {
+    "company_id": None,
+    "sent": None,
+    "clicks": None,
+    "number_phished": None,
+    "phished": []
+}
+
+p = {
     "username": None,
     "password": None,
     "time": None,
     "cookie_id": None,
-    "session_id": None
+    "session_id": None,
 }
 
-db = json.load(open("website/demo_db.json", "r"))
 
-db["company_id"] = 1
-db["clicks"] = len(users)
-db["number_phished"] = len(users)
 
-for i in range(len(users)):
-    item = copy.deepcopy(user_entry)
-    item["username"] = users[i]
-    item["password"] = "secret"
-    item["time"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    item["cookie_id"] = random.randint(0, 999999999)
-    item["session_id"] = random.randint(0, 999999999)
-    db["phished"].append(item)
+db = json.loads("[]")
 
-json.dump(db, open("website/demo_db.json", "w"))
+for i in range(COMP_NUMB):
+    comp = json.loads(json.dumps(c))
+    emp = random.randint(COMP_USER_MIN, COMP_USER_MAX)
+    comp["company_id"] = f"company_{i}"
+    comp["sent"] = emp
+    comp["clicks"] = 0
+    comp["number_phished"] = 0
+    for j in range(emp):
+        if(random.random() < EMP_CLICK_RATE):
+            comp["clicks"] += 1
+            if(random.random() < EMP_PHISH_RATE):
+                comp["number_phished"] += 1
+                phi = json.loads(json.dumps(p))
+                phi["username"] = f"user_{j}"
+                phi["password"] = "secret"
+                phi["time"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                phi["cookie_id"] = random.randint(0, 999999999)
+                phi["session_id"] = random.randint(0, 999999999)
+                comp["phished"].append(phi)
+    db.append(comp)
+
+json.dump(db, open("database.json", "w"))
---
+++ demo/inc/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+    <div class="header__first">
+        <img src="img/logo_main.png" alt="" class="header__first__img">
+    </div>
+    <div class="header__second">
+        <ul class="header__second_ul">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div class="header__third">
+        <h1 class="header__third__temp">PenTest Demo</h1>
+    </div>
+</div>
